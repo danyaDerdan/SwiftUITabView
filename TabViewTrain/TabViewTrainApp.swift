6581b61f9415e92e2178cbc31fb3bdc7b0273c9a
danyaDerdan <110595066+danyaDerdan@users.noreply.github.com>
2024-11-05 08:41:00 +0300

    Initial Commit

---
+++ TabViewTrain/TabViewTrainApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabViewTrainApp.swift
+//  TabViewTrain
+//
+//  Created by Данил Толстиков on 05.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct TabViewTrainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TabViewTrainTests/TabViewTrainTests.swift
@@ -0,0 +1,17 @@
+//
+//  TabViewTrainTests.swift
+//  TabViewTrainTests
+//
+//  Created by Данил Толстиков on 05.11.2024.
+//
+
+import Testing
+@testable import TabViewTrain
+
+struct TabViewTrainTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
